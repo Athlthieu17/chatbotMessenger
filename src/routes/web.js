0bef96f90f07923573bd950ef683dfc085f8a15f
Athlthieu17 <hieult.hust170703@gmail.com>
2023-07-15 12:23:25 +0700

    update

---
+++ src/routes/web.js
@@ -0,0 +1,14 @@
+import express from "express";
+import chatbotControllers  from "../controllers/chatbotControllers";
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get("/", chatbotControllers.getHomePage)
+
+    router.get("/webhook", chatbotControllers.getWebhook);
+    router.post("/webhook", chatbotControllers.postWebhook);
+    
+    return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
